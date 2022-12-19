c8fca4a0871177c6c708f79e90a9e52897289848
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-19 22:19:03 +0530

    timestamps added

--- models/order.js
+++ models/order.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     orderId: { type: String, required: true },
-    orderTimeDate: { type: String, required: true },
+    // orderTimeDate: { type: String, required: true },
     storeLocation: { type: String, required: true },
     city: { type: String, required: true },
     storePhone: { type: Number, required: true },
     totalItems: { type: Number, required: true },
     price: { type: Number, required: true },
     status: { type: String, default: "Ready to Pickup" }
-})
+}, { timestamps: true })
 
 module.exports = mongoose.model('orders', orderSchema);
